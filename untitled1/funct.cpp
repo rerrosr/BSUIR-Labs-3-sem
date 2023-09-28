0b270c50cd8bbcc134dc5b10f62484682d0421b6
Feona <108477623+rerrosr@users.noreply.github.com>
2023-09-28 16:43:52 +0300

    Update funct.cpp

--- untitled1/funct.cpp
+++ untitled1/funct.cpp
@@ -5,6 +5,10 @@ void addTask(std::vector<Task>& tasks){
     std::cin.ignore();
     std::cout << "Enter task text:";
     std::getline(std::cin, text);
+    if (text.empty()){
+        std::cout << "Try again" << std::endl;
+        return;
+    }
     tasks.push_back(Task(text));
 }
 
@@ -41,3 +45,49 @@ void markTaskCompleted(std::vector<Task>& tasks, size_t index){
         std::cout << "Invalid task number" << std::endl;
     }
 }
+
+void removeTask(std::vector<Task>& tasks, size_t index){
+    if (index > 0 && index <= tasks.size()) {
+        tasks.erase(tasks.begin() + index - 1);
+        std::cout << "Task removed successfully." << std::endl;
+    } else {
+        std::cout << "Invalid task number" << std::endl;
+    }
+}
+
+void editTask(Task& task){
+    std::cout << "Editing task: " << task.getText() << std::endl;
+    std::cout << "Enter new task text: ";
+    std::string newText;
+    std::cin.ignore();
+    std::getline(std::cin, newText);
+    if (!newText.empty()) {
+        task.setText(newText);
+        task.setData(std::time(nullptr)); // Обновляем дату и время при изменении задачи
+        std::cout << "Task updated successfully." << std::endl;
+    } else {
+        std::cout << "Task text cannot be empty. Task not updated." << std::endl;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Task& task){
+    os << "Task text:" << task.text << std::endl;
+    os << "Date and time:" << std::asctime(std::localtime(&task.data));
+    os << "Completed:" << (task.isCompleted() ? "Yes" : "No");
+    return os;
+}
+std::istream& operator>>(std::istream& is, Task& task){
+    std::string text;
+    std::cout<< "Enter task text: ";
+    is.ignore();
+    std::getline(is, text);
+    if (text.empty()){
+        std::cout << "Try again" << std::endl;
+        return is;
+    }
+    task.text = text;
+    task.data = std::time(nullptr);
+    task.completed = false;
+    return is;
+}
+
